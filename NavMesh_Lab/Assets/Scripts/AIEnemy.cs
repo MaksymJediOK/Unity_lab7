a476eb8026f3bb63442fb9c02cd7b68a1edecf7b
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2022-12-03 02:50:57 +0200

    NavMesh and moving

---
+++ NavMesh_Lab/Assets/Scripts/AIEnemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIEnemy : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform Player;
+    public float range = 10;
+    public Vector3 startpos;
+    void Start()
+    {
+        startpos = transform.position;
+        enemy = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        if (Vector3.Distance(Player.position, transform.position) <= range)
+            enemy.SetDestination(Player.position);
+        else
+            enemy.SetDestination(startpos);
+    }
+}
